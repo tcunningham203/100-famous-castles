2f3c60e21568c726728e415a3da67c6564edbd1b
tcunningham203 <tcunningham203@gmail.com>
2023-04-29 06:49:25 +0900

    50 photoshopped stamps

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -11,7 +11,7 @@ router.get('/', withAuth, async (req, res) => {
 
     const users = userData.map((project) => project.get({ plain: true }));
 
-    res.render('stamps', {
+    res.render('map', {
       users,
       logged_in: req.session.logged_in,
     });
