d45fc26814ef0611b03cb7ba9aba28d1181bc1ae
tcunningham203 <tcunningham203@gmail.com>
2023-05-03 08:29:43 +0900

    stamp book book css

---
+++ public/js/bookflip.js
@@ -0,0 +1,54 @@
+var right = document.getElementsByClassName("right");
+    var si = right.length;
+    var z=1;
+
+    document.getElementById("leftbutton").disabled = true;
+    
+
+
+    function disableCheck(){
+      if (si==right.length){
+        document.getElementById("leftbutton").disabled = true;
+
+      }
+      if (si==1){
+        document.getElementById("rightbutton").disabled = true;
+      }
+
+
+    }
+    
+    function turnRight()
+    {
+        if(si>=1){
+          document.getElementById("leftbutton").disabled = false;
+          document.getElementById("rightbutton").disabled = false;
+            si--;
+            document.getElementById("rightbutton").disabled = true;
+            setTimeout(function(){document.getElementById("rightbutton").disabled = false;},300);
+            disableCheck
+        }
+        else{
+          document.getElementById("rightbutton").disabled = true;
+        }
+        right[si].classList.add("flip");
+        z++;
+        right[si].style.zIndex=z;
+    }
+    function turnLeft()
+    {disableCheck
+        if(si<right.length){
+          document.getElementById("leftbutton").disabled = false;
+          document.getElementById("rightbutton").disabled = false;
+            si++;
+            document.getElementById("leftbutton").disabled = true;
+            setTimeout(function(){document.getElementById("leftbutton").disabled = false;},300);
+            
+        }
+        else{
+          document.getElementById("leftbutton").disabled = true;
+           
+        }
+        right[si-1].className="right";
+        setTimeout(function(){right[si-1].style.zIndex="auto";},350);
+    }
