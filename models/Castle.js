363ffd60946d428ef6e461f08b9e7aba429e5749
frankolms <frankolms@gmail.com>
2023-05-01 21:35:17 -0400

    added castle model, castle routes, added castle model and castle data to homeroutes, imported castleroutes to index.js in api folder

---
+++ models/Castle.js
@@ -0,0 +1,64 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Castle extends Model {}
+
+Castle.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    stampnumber: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    nameen: {
+      type: DataTypes.STRING,
+    },
+    namejp: {
+      type: DataTypes.STRING,
+    },
+    castlepic: {
+      type: DataTypes.STRING,
+    },
+    stamppic: {
+      type: DataTypes.STRING,
+    },
+    region: {
+      type: DataTypes.STRING,
+    },
+    prefecture: {
+      type: DataTypes.STRING,
+    },
+    city: {
+      type: DataTypes.STRING,
+    },
+    type: {
+      type: DataTypes.STRING,
+    },
+    condition: {
+      type: DataTypes.STRING,
+    },
+    access: {
+      type: DataTypes.STRING,
+    },
+    website: {
+      type: DataTypes.STRING,
+    },
+    history: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "castle",
+  }
+);
+
+module.exports = Castle;
--- models/index.js
+++ models/index.js
@@ -1,3 +1,4 @@
-const User = require('./User');
+const User = require("./User");
+const Castle = require("./Castle");
 
-module.exports = { User };
+module.exports = { User, Castle };
