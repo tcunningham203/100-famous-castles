d0a8ad3b7dc3b27f6fc4c6291806c5c5c00fabe3
Lauren Sullivan <122634162+lnsvn@users.noreply.github.com>
2023-05-02 20:39:05 -0400

    creates connection between map button and map page

--- public/js/pages.js
+++ public/js/pages.js
@@ -1,3 +1,5 @@
 const stampbookBtn = document.getElementById('stamps');
+const mapPageBtn = document.getElementById('map');
 
 const renderStampsPage = () => window.location = '/stampbook';
+const renderMapPage = () => window.location = '/';
