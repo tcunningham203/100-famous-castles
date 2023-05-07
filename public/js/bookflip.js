f788af435464284e0a5cf1d5792bb14d95ab1c70
tcunningham203 <tcunningham203@gmail.com>
2023-05-07 14:37:46 +0900

    media query fixes

--- public/js/bookflip.js
+++ public/js/bookflip.js
@@ -48,3 +48,49 @@ function turnLeft() {
     right[si - 1].style.zIndex = "auto";
   }, 350);
 }
+
+
+function disableCheck2() {
+  if (si == right.length) {
+    document.getElementById("leftbutton2").disabled = true;
+  }
+  if (si == 1) {
+    document.getElementById("rightbutton2").disabled = true;
+  }
+}
+
+function turnRight2() {
+  if (si >= 1) {
+    document.getElementById("leftbutton2").disabled = false;
+    document.getElementById("rightbutton2").disabled = false;
+    si--;
+    document.getElementById("rightbutton2").disabled = true;
+    setTimeout(function () {
+      document.getElementById("rightbutton2").disabled = false;
+    }, 400);
+    disableCheck2;
+  } else {
+    document.getElementById("rightbutton2").disabled = true;
+  }
+  right[si].classList.add("flip");
+  z++;
+  right[si].style.zIndex = z;
+}
+function turnLeft2() {
+  disableCheck2;
+  if (si < right.length) {
+    document.getElementById("leftbutton2").disabled = false;
+    document.getElementById("rightbutton2").disabled = false;
+    si++;
+    document.getElementById("leftbutton2").disabled = true;
+    setTimeout(function () {
+      document.getElementById("leftbutton2").disabled = false;
+    }, 400);
+  } else {
+    document.getElementById("leftbutton2").disabled = true;
+  }
+  right[si - 1].className = "right";
+  setTimeout(function () {
+    right[si - 1].style.zIndex = "auto";
+  }, 350);
+}
