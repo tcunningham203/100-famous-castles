28a27aefac4aced1c8052f67bdf29b387a5479c0
Lauren Sullivan <122634162+lnsvn@users.noreply.github.com>
2023-05-08 18:39:35 -0400

    adds alert when tip is added

--- public/js/noteposting.js
+++ public/js/noteposting.js
@@ -36,6 +36,7 @@ document.addEventListener('DOMContentLoaded', () => {
         // Reset the input field
         document.getElementById('commentText').value = '';
         document.location.reload();
+        alert(`Tip added successfully!`)
       } else {
         console.log('Failed to create comment:', response.status);
       }
