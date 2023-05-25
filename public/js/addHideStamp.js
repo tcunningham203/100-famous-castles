7b4f6060e48f9f419c88f66478feb9ad36e1fc1d
tcunningham203 <tcunningham203@gmail.com>
2023-05-26 01:19:18 +0900

    comments to handlebars pages and removed modal on stamppage

--- public/js/addHideStamp.js
+++ public/js/addHideStamp.js
@@ -3,7 +3,6 @@ document.addEventListener("DOMContentLoaded", () => {
   let selectedBox = null;
 
   document.querySelectorAll(".stamp-box").forEach(function (box) {
-    const modal = new bootstrap.Modal(document.getElementById("delete-modal"));
     box.addEventListener("click", function () {
       if (!isAnimating) {
         isAnimating = true;
@@ -19,49 +18,76 @@ document.addEventListener("DOMContentLoaded", () => {
       e.stopPropagation();
       if (!isAnimating) {
         selectedBox = $(this).closest(".stamp-box");
-        modal.show();
-        modal._element
-          .querySelector(".btn-danger")
-          .addEventListener("click", function () {
-            modal.hide();
-            if (selectedBox && !isAnimating) {
-              const currentImg = selectedBox.find("img");
-              isAnimating = true;
-              currentImg.addClass("fade-out");
-              setTimeout(function () {
-                isAnimating = false;
-                currentImg.addClass("hidden");
-                currentImg.removeClass("fade-out puff-in-center");
-              }, 2000);
-              selectedBox = null;
-            }
-          });
-        modal._element
-          .querySelector(".btn-success")
-          .addEventListener("click", function () {
-            modal.hide();
-            selectedBox = null;
-          });
+        deleteStamp();
       }
     });
   });
 
-//Sudar
-  //     connection.query(
-  //       "SELECT COUNT(CASTLE_ID) FROM STAMP GROUP BY (USER_ID)",
-  //       (err, rows) => {
-  //         // When done, release connection
-  //         connection.release();
-  //         if (!err) {
-  //           res.render("edituser", { rows });
-  //         } else {
-  //           console.log(err);
-  //         }
-  //         console.log("Data from students table: \n", rows);
-  //       }
-  //     );
-  //   });
-  // };
-
-  //Sudar
+  const deleteStamp = async () => {
+    try {
+      if (selectedBox && !isAnimating) {
+        const currentImg = selectedBox.find("img");
+        isAnimating = true;
+        currentImg.addClass("fade-out");
+        setTimeout(function () {
+          isAnimating = false;
+          currentImg.addClass("hidden");
+          currentImg.removeClass("fade-out puff-in-center");
+         
+          selectedBox = null;
+        }, 2000);
+      }
+    } catch (error) {
+      console.log("An error occurred:", error);
+    }
+  };
 });
+
+
+
+// DEVELOPER NOTE: This is an alternate version of the code featuring a modal. This modal was meant to show up to ensure the user would not accidentally delete a stamp, so as to save their "date stamped" information. However, this feature was removed, so the modal is no longer needed. But we intend to bring this feature back in the future, and are therefore keeping this code here.
+
+
+// document.querySelectorAll(".stamp-box").forEach(function (box) {
+//   const modal = new bootstrap.Modal(document.getElementById("delete-modal"));
+//   box.addEventListener("click", function () {
+//     if (!isAnimating) {
+//       isAnimating = true;
+//       $(this).find("img").addClass("puff-in-center");
+//       setTimeout(() => {
+//         $(this).find("img").removeClass("hidden");
+//         isAnimating = false;
+//       }, 350);
+//     }
+//   });
+
+//   box.querySelector("img").addEventListener("click", function (e) {
+//     e.stopPropagation();
+//     if (!isAnimating) {
+//       selectedBox = $(this).closest(".stamp-box");
+//       modal.show();
+//       modal._element
+//         .querySelector(".btn-danger")
+//         .addEventListener("click", function () {
+//           modal.hide();
+//           if (selectedBox && !isAnimating) {
+//             const currentImg = selectedBox.find("img");
+//             isAnimating = true;
+//             currentImg.addClass("fade-out");
+//             setTimeout(function () {
+//               isAnimating = false;
+//               currentImg.addClass("hidden");
+//               currentImg.removeClass("fade-out puff-in-center");
+//             }, 2000);
+//             selectedBox = null;
+//           }
+//         });
+//       modal._element
+//         .querySelector(".btn-success")
+//         .addEventListener("click", function () {
+//           modal.hide();
+//           selectedBox = null;
+//         });
+//     }
+//   });
+// });
--- public/js/noteposting.js
+++ public/js/noteposting.js
@@ -41,36 +41,3 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-// document.addEventListener('DOMContentLoaded', () => {
-//   const commentForm = document.getElementById('commentForm');
-
-//   commentForm.addEventListener('submit', async (e) => {
-//     e.preventDefault();
-
-//     try {
-//       const commentText = document.getElementById('commentText').value;
-//       const castleId = window.location.pathname.split('/').pop();
-
-//       const response = await fetch(`/api/notes/${castleId}`, {
-//         method: 'POST',
-//         body: JSON.stringify({ content: commentText }),
-//         headers: { 'Content-Type': 'application/json' },
-//       });
-
-//       if (response.ok) {
-//         document.getElementById('commentText').value = '';
-
-//         $("#tyvm1").toast("show");
-//         setTimeout(function () {
-//           window.location.href = window.location.pathname + "#nav-profile"; // Redirect to notes tab
-//         }, 1200);
-//       } else {
-//         console.log('Failed to create comment:', response.status);
-//       }
-//     } catch (error) {
-//       console.log('Error:', error);
-//     }
-//   });
-// });
-
- 
